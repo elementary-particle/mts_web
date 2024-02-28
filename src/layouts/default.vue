ac5c521c7fb28c717aa93d5a5db41eb87e555291
Kuchinashi Hoshikawa <kuchinashi@ahympia.com>
2024-02-29 02:41:54 +0800

    Minor Changes on Editor Layout & Design

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -19,7 +19,21 @@
 import DefaultBar from "./default/AppBar.vue";
 import DefaultView from "./default/View.vue";
 
+import { onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+
 import { useAppStore } from "@/store/app";
 
 const app = useAppStore();
+
+const { locale } = useI18n();
+
+onMounted(async () => {
+  let l: string = localStorage.lang ? localStorage.lang : navigator.language;
+  if (l.startsWith("zh")) {
+    locale.value = "zhHans";
+  } else if (l.startsWith("en")) {
+    locale.value = "en";
+  }
+});
 </script>
